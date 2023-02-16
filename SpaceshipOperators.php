7af167a110da50c256453539374d8c041f958902
ajpHava <113887498+ajpHava@users.noreply.github.com>
2023-02-16 13:09:17 +0530

    -m ""

--- SpaceshipOperators.php
+++ SpaceshipOperators.php
@@ -1,5 +1,10 @@
 <?php
-    $num1 = 12;
+    // If left operand  is greater then result is 1
+    // If right operand  is greater then result is -1
+    // If both are equal then result is 0
+    $num1 = 11;
     $num2 = 12;
-    echo ($num1<=>$num2);
+    echo ($num1<=>$num2);  //O/P : 1
+    var_dump($num1>$num2);
+    echo ($num1>$num2); // empty result.
 ?>
